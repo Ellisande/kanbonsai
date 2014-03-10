440c639c06bb37537439fe66e75378d514dd1c14
unknown <justin.dragos@gmail.com>
2014-03-09 23:42:13 -0700

    Nothing special.

--- app/js/services.js
+++ app/js/services.js
@@ -28,9 +28,9 @@ app
       })
     },
     connect: function(){
-//      socket = io.connect('http://www.ellisande.com:3000/');
       if(!socket){
-          socket = io.connect('http://localhost:3000/');
+          socket = io.connect('http://www.ellisande.com:3000/');
+//          socket = io.connect('http://localhost:3000/');
       } else {
         //socket.socket.connect();
       }
