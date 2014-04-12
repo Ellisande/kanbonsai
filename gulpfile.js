65abf136c4cf99f5036497339ae5db3a343d67e1
Ellisande <justin.dragos@gmail.com>
2014-04-11 23:07:22 -0700

    Adding in several new gulp tasks.

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify-css');
+var jshint = require('gulp-jshint');
+var csslint = require('gulp-csslint');
+
+gulp.task('js-minify', function(){
+    return gulp.src('app/js/**/*.js')
+        .pipe(concat('app.js'))
+//        .pipe(uglify())
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('js-lint', function(){
+    return gulp.src('app/js/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('css-minify', function(){
+    return gulp.src(['app/css/**/*.css'])
+        .pipe(concat('app.css'))
+        .pipe(minify())
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('css-lint', function(){
+    return gulp.src('app/css/**/*.css')
+        .pipe(csslint())
+        .pipe(csslint.reporter('default'));
+});
+
+
+
+gulp.task('node', function(){
+  nodemon({ 
+      script: 'coffee-server.js', 
+      ext: 'js', 
+      ignore: ['app/**/*.js','config/**/*.js','test/**/*.js'] }
+    )
+    .on('restart', function () {
+      console.log('restarted!');
+    });
+});
+
+gulp.task('lint', ['js-lint', 'css-lint']);
+
+gulp.task('minify', ['css-minify','js-minify']);
+
+gulp.task('watch', function(){
+    gulp.watch('app/js/**/*.js', ['minify', 'js-lint']);
+    gulp.watch('app/css/**/*.css', ['css-lint']);
+});
+
+gulp.task('default', ['node','minify', 'lint','watch']);
