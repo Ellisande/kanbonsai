d30aed25c18e92c5598a629bf3a67554eaba2f30
Ellisande <justin.dragos@gmail.com>
2014-04-11 23:13:38 -0700

    Lint fixes.

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,9 +1,10 @@
-'use strict';
+
 
 /* Filters */
 
 angular.module('filters', []).filter('timer', function(){
     return function(input){
+      'use strict';
       return input < 10 ? '0'+input : ''+input;  
     };
 });
