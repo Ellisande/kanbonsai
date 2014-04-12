69cd8cb7392001da2833c2a40a713ef556f9ad72
Ellisande <justin.dragos@gmail.com>
2014-04-11 23:12:31 -0700

    Lint fixes. Removed some dead code.

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,12 @@
-'use strict';
+
 
 /* App Module */
-
 var app = angular.module('lean-coffee', ['filters','services']).
-  config(['$routeProvider', function($routeProvider) {
+  config(function($routeProvider) {
+  'use strict';
   $routeProvider.
-      //Removed temporarily while focusing on single meeting principle.
       when('/home', {templateUrl: 'partials/home.html',   controller: HomeCtrl}).
       when('/meeting/:meetingName', {templateUrl: 'partials/meeting.html', controller: MeetingCtrl}).
-      //Removed temporarily while focusing on single meeting principle.
       when('/snapshot', {templateUrl: "partials/snapshot.html", controller: SnapshotCtrl}).
-      // when('/snapshot', {tempalteUrl: "partials/snapshot.html", controller: CreateCtrl}).
       otherwise({redirectTo: '/home'});
-}]);
+});
