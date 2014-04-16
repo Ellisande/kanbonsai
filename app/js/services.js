f6de5782e1b5afd0897e36527e773bc6af19711e
Ellisande <justin.dragos@gmail.com>
2014-04-15 23:58:52 -0700

    Added a new local timer service. Also changed the web socket connection function to be self invoking.

--- app/js/services.js
+++ app/js/services.js
@@ -4,36 +4,37 @@ angular.module('services', [])
 .factory('socket', function ($rootScope) {
     'use strict';
     var socket;
+    
+    var connect = function(){
+          if(!socket){
+            socket = io.connect('http://localhost:3000/');
+          }
+    };
+    
+    connect();
+    
     return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {  
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-        socket.emit(eventName, data, function () {
+        on: function (eventName, callback) {
+          socket.on(eventName, function () {  
             var args = arguments;
-                $rootScope.$apply(function () {
-                    if (callback) {
-                        callback.apply(socket, args);
-                }
+            $rootScope.$apply(function () {
+              callback.apply(socket, args);
             });
-      });
-    },
-    connect: function(){
-      if(!socket){
-    //          socket = io.connect('http://www.ellisande.com:3000/');
-          socket = io.connect('http://localhost:3000/');
-      } else {
-        //socket.socket.connect();
-      }
-    },
-    disconnect: function(){
-      socket.disconnect();
-    }
+          });
+        },
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                    $rootScope.$apply(function () {
+                        if (callback) {
+                            callback.apply(socket, args);
+                    }
+                });
+          });
+        },
+        disconnect: function(){
+          socket.disconnect();
+        }
     };
 }).
 factory('snapshot', function (){
@@ -43,7 +44,6 @@ factory('snapshot', function (){
       this.capture = angular.copy(meeting);
     },
     get: function(){
-      console.log("On the retrieve");
       console.log(this.capture);
       var temp = this.capture;
       this.capture = {};
@@ -104,7 +104,48 @@ factory('timerService', function(socket, $timeout){
         });
         return timer;   
     };
-});
+}).factory('localTimer', function($timeout){
+    return function($scope){
+        $scope.duration = moment.duration(3, 'minutes');
+        var currentTimeout = {};
+        
+        var startTimeout = function(timer){
+            var onTimeout = function(){
+                timer.expired = moment().isAfter(timer.endTime);
+                if(timer.expired){
+                    $scope.duration = moment.duration(3, 'minutes');
+                } else {
+                    $scope.duration = timer.currentDuration();
+                    currentTimeout = $timeout(onTimeout,1000);
+                }
+            };
+            currentTimeout = $timeout(onTimeout,1000);
+        };
+        
+        var timer = {
+            endTime: moment(),
+            currentDuration: function(){
+              return moment.duration(this.endTime.diff(moment()));  
+            },
+            start: function(duration){
+                $timeout.cancel(currentTimeout);
+                timer.endTime = moment().add(duration, 'milliseconds');
+                startTimeout(timer);
+                timer.expired = false;
+            },
+
+            stop: function(){
+                $scope.duration = moment.duration(3,'minutes');
+                $timeout.cancel(currentTimeout);
+                timer.expired = true;
+                socket.emit('timer:stop',{});
+            },
+
+            expired: true
+        };
+        
+        return timer; 
+}});
 
 function Meeting(name, startTime){
 	this.name = name;
