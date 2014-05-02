25e08aae5f98de315f437e79a7169baaac4385b3
Mayor McCheese <justin.dragos.m91s@statefarm.com>
2014-05-02 10:10:20 -0700

    Changed the default socket connec to be port 5000.

--- app/js/services.js
+++ app/js/services.js
@@ -7,7 +7,7 @@ angular.module('services', [])
     
     var connect = function(){
           if(!socket){
-            socket = io.connect('http://localhost:3000/');
+            socket = io.connect('http://localhost:5000/');
           }
     };
     
