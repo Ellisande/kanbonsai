849bc3217031c53b623fd6118ec3a61e916cb3a4
Steve Dunstan <steve.dunstan.kskj@statefarm.com>
2014-06-02 16:38:34 -0700

    added safety checks for timer

--- server/server-meeting.js
+++ server/server-meeting.js
@@ -111,7 +111,19 @@ module.exports = function ServerMeeting(name) {
     };
 
     this.getTimer = function(){
-      if(this.phase == phases.discuss) return this.getDiscussPhaseTimer();
-      return this.getRegularTimer();
+      var timer;
+      if(this.phase == phases.discuss) {
+        timer = this.getDiscussPhaseTimer();
+      } else {
+        timer = this.getRegularTimer();
+      }
+
+      // All this saftey checking is a workaround until we figure out why
+      // this method sometimes returns a non-duration object.
+      if (!timer || typeof timer.asMilliseconds != 'function') {
+        timer = moment.duration(0, 'seconds');
+      }
+
+      return timer;
     };
 };
