776992e7acdf23f7f7379428c9024bc4600f9ee3
Mayor McCheese <justin.dragos.m91s@statefarm.com>
2014-06-18 20:15:07 -0700

    Added in more names to get to the target 50. Still seeking more names!

--- server/model.js
+++ server/model.js
@@ -1,9 +1,10 @@
 var allNames = [
   'Mayor McCheese','Hamburgler','Strawberry Shortcake','Xena, Warrior Princess','Kruul the Warrior King','Hippie','Mr. Drippy',
-  'Master Shake','Grimmace','Sailor Moon','Blue','A Fish Called Wonda','Resivior Dog','Marsellus Wallus','Tony Stark','Justin Beiber',
+  'Master Shake','Grimmace','Sailor Moon','Blue','A Fish Called Wonda','Mr. Pink','Marsellus Wallus','Tony Stark','Justin Beiber',
   'Bubble Puppy','Dora the Explorer','Spongebob Squarepants','Moonpie','Smores','Onyx','Uncle Bob','Godzilla',
   'McLovin','Sparky, the Fire Breathing Chameleon','The Tin Man', 'Magic Mike', 'Squared', 'MD', 'Darth Helemet', 'President Scroob',
-  'Glass Popcorn', 'Kobe', 'Pierre the Pelican', 'The Dunstan', 'Secret Squirrel', 'Luigi'
+  'Glass Popcorn', 'Kobe', 'Pierre the Pelican', 'The Dunstan', 'Secret Squirrel', 'Luigi', 'Yoda', 'Jar Jar Binks',
+  'Nemo', 'Dory', 'Toothless', 'Hiccup', 'Professor X', 'Dark Phoenix', 'Not a Bug', 'A Feature', '.Troll', 'Vincent Vega'
 ];
 
 var randomName = function(allNames){
