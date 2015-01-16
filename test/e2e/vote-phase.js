03be12386d0856b99b948a5a7bb5974418184779
Mayor McCheese <justin.dragos.m91s@statefarm.com>
2015-01-16 12:27:59 -0700

    Added test to ensure refreshing does not reset vote count.

--- test/e2e/vote-phase.js
+++ test/e2e/vote-phase.js
@@ -6,33 +6,32 @@ describe('lean coffee', function() {
   describe('voting phase', function(){
 
     var votingPage = new po.VotingPage();
-    var meetingPage = new po.MeetingPage();
 
-    it('should allow you to navigate to the voting phase', function() {
+   it('should allow you to navigate to the voting phase', function() {
       // global.goToNextPhase();
       votingPage.bypassEarlierPages();
       expect(global.getPhaseText()).toMatch(/PHASE: VOTING/);
     });
 
-    it('should display an icon to vote up a topic', function(){
+   it('should display an icon to vote up a topic', function(){
       expect(element(by.repeater('topic in meeting.topics').row(0)).$('.voteUp img').isPresent()).toBe(true);
     });
 
-    it('should display an icon to vote down a topic', function(){
+   it('should display an icon to vote down a topic', function(){
       expect(element(by.repeater('topic in meeting.topics').row(0)).$('.voteDown img').isPresent()).toBe(true);
     });
 
-    it('should display the number of votes the user has remaining', function(){
+   it('should display the number of votes the user has remaining', function(){
       expect(global.getElementById('votesRemaining').getText()).toBe('You have 3 votes remaining.');
     });
 
-    it('should allow a user to vote', function(){
+   it('should allow a user to vote', function(){
       votingPage.voteUp(0);
       expect(global.getElementById('votesRemaining').getText()).toContain('2');
       expect(votingPage.getNumberOfVotesForTopic(0)).toContain('1');
     });
 
-    it('should allow a user to vote up to 3 times', function(){
+   it('should allow a user to vote up to 3 times', function(){
       votingPage.voteUp(1);
       expect(votingPage.getNumberOfVotesForTopic(1)).toContain('1');
       expect(global.getElementById('votesRemaining').getText()).toContain('1');
@@ -40,14 +39,14 @@ describe('lean coffee', function() {
       expect(global.getElementById('votesRemaining').getText()).toBe('You have no votes remaining.');
     });
 
-    it('should allow a user to change their votes', function(){
+   it('should allow a user to change their votes', function(){
       votingPage.voteDown(1);
       expect(global.getElementById('votesRemaining').getText()).toContain('1');
       votingPage.voteDown(1);
       expect(global.getElementById('votesRemaining').getText()).toContain('2');
     });
 
-    it('should allow a user to vote on up to 3 topics', function(){
+   it('should allow a user to vote on up to 3 topics', function(){
       votingPage.voteUp(1);
       expect(global.getElementById('votesRemaining').getText()).toContain('1');
       votingPage.voteUp(2);
@@ -57,7 +56,7 @@ describe('lean coffee', function() {
       expect(votingPage.getNumberOfVotesForTopic(2)).toContain('1');
     });
 
-    it('should allow a user to vote 3 times on a single topic', function(){
+   it('should allow a user to vote 3 times on a single topic', function(){
       votingPage.voteDown(1);
       expect(global.getElementById('votesRemaining').getText()).toContain('1');
       expect(votingPage.getNumberOfVotesForTopic(1)).toContain('0');
@@ -70,7 +69,7 @@ describe('lean coffee', function() {
       expect(votingPage.getNumberOfVotesForTopic(0)).toContain('3');
     });
 
-    it('should display total number of remaining votes across all users to the host', function(){
+   it('should display total number of remaining votes across all users to the host', function(){
       expect(global.getElementById('roomVotesRemaining').getText()).toBe('There are no votes remaining in the room.');
       votingPage.voteDown(0);
       expect(global.getElementById('roomVotesRemaining').getText()).toBe('There is 1 vote remaining in the room.');
@@ -81,7 +80,7 @@ describe('lean coffee', function() {
 
     });
 
-    it('should not display total number of remaining votes across all users to the normal users', function(){
+   it('should not display total number of remaining votes across all users to the normal users', function(){
       var becomeHost = global.getElemByButtonText('Become a Host');
       var backToNormalUser = global.getElemByButtonText('Back to Normal User');
 
@@ -93,7 +92,7 @@ describe('lean coffee', function() {
       becomeHost.click();
     });
 
-    it('should not allow a user to vote 4 or more times', function(){
+   it('should not allow a user to vote 4 or more times', function(){
       votingPage.voteUp(0);
       votingPage.voteUp(0);
       votingPage.voteUp(0);
@@ -103,9 +102,9 @@ describe('lean coffee', function() {
       var voteUpElement = element(by.repeater('topic in meeting.topics').row(0)).$('.vote').$('.voteUp');
       expect(voteUpElement.getAttribute('class')).toContain('hidden');
     });
-		
-		it('should not reset amount of votes on refresh', function(){
-			browser.get('http://localhost:5000/#/meeting/' + meetingPage.meetingName);
+
+	it('should not reset amount of votes on refresh', function(){
+			browser.get('http://localhost:5000/#/meeting/' + votingPage.meetingName);
       expect(global.getElementById('votesRemaining').getText()).toBe('You have no votes remaining.');
       expect(votingPage.getNumberOfVotesForTopic(0)).toContain('3');
 		});
